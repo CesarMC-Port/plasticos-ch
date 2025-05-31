d4683a92446f7fb6de611a1e18789eb7b112a923
CesarMC-Port <morecontrol64@gmail.com>
2025-05-31 12:22:23 -0400

    arreglar uso movil

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -31,6 +31,13 @@ const Calculator: React.FC = () => {
       ...prevState,
       selectedProduct: product
     }));
+
+    setTimeout(() => {
+      const target = document.getElementById('selected-quantity');
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 200);
   };
   
   const handleQuantityChange = (quantity: number) => {
@@ -97,18 +104,25 @@ const Calculator: React.FC = () => {
                   </h2>
                 </div>
               </div>
-              
-              <button
-                onClick={handleReset}
-                className="w-full py-4 px-6 text-lg border-2 border-gray-300 rounded-xl text-gray-700 bg-white hover:bg-gray-50 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-200"
-              >
-                Seleccionar otro producto
-              </button>
             </div>
+            <button
+              onClick={handleReset}
+              className="
+                w-[90%] py-4 px-6 text-lg border-2 border-white rounded-xl text-white
+                bg-blue-900 hover:bg-blue-600 hover:border-white focus:outline-none focus:ring-2 
+                focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-200 fixed md:relative top-[20px] md:top-[0]
+                flex flex-row justify-around items-center left-[5%] z-[100]
+              "
+            >
+              Seleccionar otro producto
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-[35px] h-[35px] fill-white" viewBox="0 0 384 512">
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+              </svg>
+            </button>
           </div>
           
           <div className="md:w-2/3 space-y-8">
-            <div className="bg-white rounded-xl shadow-lg p-8">
+            <div className="bg-white rounded-xl shadow-lg p-8" id="selected-quantity">
               <QuantitySelector
                 quantity={state.quantity}
                 onChange={handleQuantityChange}
